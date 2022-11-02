ddd3fc9847241cffe118aafc030f6fff2c7c90b2
laurens <laurens@shockmedia.nl>
2022-11-02 14:48:56 +0100

    First changes to database connection

--- index.php
+++ index.php
@@ -16,24 +16,15 @@ $isserver = 0;
 mysql_connect("localhost", $db_user, $db_pass);
 
 $query_checkifdbexists = 'SELECT IF(EXISTS(SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = "' . $db_name . '"), 1, 0)AS doesexist';
-$result = mysql_query($query_checkifdbexists) or die (mysql_error());
-$db_does_exist = 0;
-while($row=mysql_fetch_assoc($result))
-{
-  $db_does_exist = $row['doesexist'];
-}
+$result = $dbConn->query($query_checkifdbexists);
+$db_does_exist = $result->fetch_row()['doesexist'];
 
 if($db_does_exist == 0) {
   header('location:./deploy/deploy.php');
 } else {
-  mysql_select_db($db_name);
-
   $query = 'SELECT * FROM isserver';
-  $result = mysql_query($query) or die(mysql_error());
-
-  while($row = mysql_fetch_array($result))   {
-    $isserver = $row['servergestart'];
-  }
+  $result = $dbConn->query($query);
+  $isserver = $result->fetch_row()['servergestart'];
 
   if($isserver == 0) {
     $_SESSION['isserver'] = 1;
--- server/db_resources.php
+++ server/db_resources.php
@@ -1,7 +1,14 @@
 <?php
+$db_host = 'localhost';
 $db_user = 'root';
 $db_pass = 'usbw';
 $db_name = 'musicplayer';
+
+$db = new mysqli($db_host,$db_user,$db_pass,$db_name);
+
+if($dbConn->connect_error) {
+	die("Database Connection Error, Error No.: ".$dbConn->connect_errno." | ".$dbConn->connect_error);
+}
 
 $musiclist = 'musiclist';
 $playlist = 'playlist';
